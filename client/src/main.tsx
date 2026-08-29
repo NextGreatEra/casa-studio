00c2c2cc60cb8735078e3261bd89d8482eb9d9fb
NextGreatEra <nextgreatera@gmail.com>
2026-08-28 20:38:23 -0700

    Add Vite React client shell, fonts CSS, and index.html.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const root = document.getElementById("root");
+if (!root) throw new Error("root missing");
+createRoot(root).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
